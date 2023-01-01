0dfcf1f762f3808306e9ce9c6ea26fca36bf1406
Erel Segal-Halevi <erelsgl@gmail.com>
2023-01-01 13:17:43 +0200

    organize web

--- 11-python-web/code/4.forms/flask_example/forms.py
+++ 11-python-web/code/4.forms/flask_example/forms.py
@@ -4,15 +4,13 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, InputRequir
 from wtforms.widgets import TextArea
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[Length(min=2, max=20)])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Submit')
+    username = StringField(label='Username', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[Length(min=2, max=20)])
+    remember = BooleanField(label='Remember Me')
+    submit = SubmitField(label='Submit')
 
 
 class DataForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     age = IntegerField("Age", validators=[InputRequired(), NumberRange(0,120)])
